840b2b7089efe461e735bb8178f1f491e200294a
Varun Puchnanda <varun.puchnanda@gmail.com>
2025-11-12 18:56:47 +0300

    Deleted vite.config.js and modified vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Ensure 'base' is set to the root path for standard hosting
+  base: '/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
+      "/api": "https://odooprodfrontend.onrender.com"
     }
   }
 })
