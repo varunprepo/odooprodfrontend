473ed32915114e8e425608035e99949c76c19fe7
Varun Puchnanda <varun.puchnanda@gmail.com>
2025-11-13 11:07:40 +0300

    Added vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // Ensure 'base' is set to the root path for standard hosting
-  base: '/',
+  //base: '/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
   },
   server: {
     proxy: {
-      "/api": "https://odooprodfrontend.onrender.com"
+      "/api": "https://odooprodbackend.onrender.com"
     }
   }
 })
